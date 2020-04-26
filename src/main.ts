9456eabb22af241ac565c2a32987fca5750cb6fd
Shelley Vohr <shelley.vohr@gmail.com>
2020-04-25 23:09:02 -0700

    refactor: don't keep global BrowserWindow ref

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ import { isDevMode } from './utils/helpers';
 // Webpack declares this, so we just need to tell TypeScript it'll be real
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
-let win: Electron.BrowserWindow | null;
-
 const createWindow = () => {
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     frame: false,
     fullscreenable: false,
     height: 700,
@@ -21,8 +19,6 @@ const createWindow = () => {
   });
 
   win.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  win.on('closed', () => { win = null; });
 };
 
 app.on('ready', async () => {
@@ -42,8 +38,8 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
   }
 });
 
